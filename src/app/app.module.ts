4c1c127487090ce7fc4dfbcae65e3d3b7d153e17
shayim <shaynv@hotmail.com>
2018-10-08 20:17:15 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import {ThreadsService} from "./services/threads.service";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserSelectionComponent,
+    ThreadSectionComponent,
+    MessageSectionComponent,
+    ThreadListComponent,
+    MessageListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [ThreadsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
